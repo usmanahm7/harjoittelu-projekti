64e0f12121a7da602a4e4b379bce848f8cf87e0b
usmanahm7 <usman.m.ahmad@metropolia.fi>
2026-08-20 15:36:20 +0300

    tehtävä

--- tuntitehta2.py
+++ tuntitehta2.py
@@ -1,3 +1,4 @@
 paiva=input("kuinka monta paivä")
 sekuntti=60*60*24*int(paiva)
 print("anettu määrä päiviä on sekunteina",sekuntti)
+hei
---
+++ tuntitehtävä3.py
@@ -0,0 +1,6 @@
+paino=float(input("kuinka monta grammaa"))
+
+kilo = int(paino // 1000)
+gramma = int(paino % 1000)
+
+print(f"Määrä kiloina ja grammoina: {kilo} kg {gramma} g")
