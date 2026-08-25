1e9b7e8ec38b47c2e8c09112b7d3c815f1f1685f
usmanahm7 <usman.m.ahmad@metropolia.fi>
2026-08-25 22:38:13 +0300

    kotitehtävä

---
+++ mod01/Kotitehtävä.3.3.py
@@ -0,0 +1,12 @@
+#3. Muuttujat ja vuorovaikutteiset ohjelmat
+#tehtävä 3.3.
+
+kanta = float(input("Anna suorakulmion kanta: "))
+
+korkeus = float(input("Anna suorakulmion korkeus"))
+
+pinta_ala = kanta * korkeus
+piiri = 2 * kanta + 2 * korkeus
+
+print(f"Suorakulmion pinta-ala: {pinta_ala}")
+print(f"Suorakulmion piiri: {piiri}")
