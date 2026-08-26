2b9c3b94d3448093433f70c1ec3ff35cd30546bf
usmanahm7 <usman.m.ahmad@metropolia.fi>
2026-08-26 14:51:35 +0300

    kotitehtävä

--- mod01/kotitehtävä.3.4..py
+++ mod01/kotitehtävä.3.4..py
@@ -1,3 +1,11 @@
-käyttäjä = input("Anna kolme kokonaislukua.: ")
-luku = float(käyttäjä)
-keskiarvo = (luku)/32
+import math
+
+a =float(input("Anna ensimäinen kokonais luku"))
+b =float(input("Anna toinen kokonais luku"))
+c =float(input("Anna kolmas kokonais luku"))
+
+summa = a+b+c
+tulo = a*b*c
+keskiarvo =  summa / 3
+
+print("Kokonaisluku on", a, "Kokonaisluku 2 on", b, "Kokonaisluku 3 on", c, "Lukujen summa on", tulo, "Lukujen keskiarvo on", keskiarvo)
