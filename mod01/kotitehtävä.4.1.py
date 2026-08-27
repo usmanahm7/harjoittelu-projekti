fb6586e89b0e6dacc22e374986dbbc8743d41ae8
usmanahm7 <usman.m.ahmad@metropolia.fi>
2026-08-27 09:59:56 +0300

    kotitehtävä

---
+++ mod01/kotitehtävä.4.1.py
@@ -0,0 +1,10 @@
+kuha = float(input("Anna kuhan pituus cm: "))
+alamitta = 37
+
+if kuha < alamitta:
+    puuttu = alamitta - kuha
+    print("Laske kuha takaisin järveen. ") 
+    print(f"{puuttu:.1f} puuttuu cm") 
+else:
+    print("Oikea kuhan pituus!")
+    
