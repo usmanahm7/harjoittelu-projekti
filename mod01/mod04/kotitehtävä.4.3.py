88cfcf5b82b3f414bd6b289a8acb568a64aa23cf
usmanahm7 <usman.m.ahmad@metropolia.fi>
2026-08-27 21:06:07 +0300

    if

--- mod01/mod04/kotitehtävä.4.3.py
+++ mod01/mod04/kotitehtävä.4.3.py
@@ -1,2 +1,22 @@
+#kysytään käyttäjältä sukupuoli.
 sukupuoli = input("Anna biologinen sukupuoli (mies/nainen): ")
-hb = int(input("Anna hemoglobiarvo: "))
+
+#kysytään heemoglobi arvoa käyttäjältä.
+hb = int(input("Anna hemoglobiarvo (g/l): "))
+
+#laitetaan arvot mitä saatin mies ja naisen paikalle.
+if sukupuoli == "mies":
+    if hb < 134:
+        print("Hemoglobi on alhainen: ")
+    elif hb <= 195:
+        print("Hemoglobi on normaali: ")
+    else:
+        print("Hemoglobi on korkea!: ")
+
+if sukupuoli == "nainen":
+    if hb < 117:
+        print("Hemoglobi on alhainen: ")
+    elif hb <= 175:
+        print("Hemoglobi on normaali: ")
+    else:
+        print("Hemoglobi on korkea!: ")
