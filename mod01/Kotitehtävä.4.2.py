49917d2f16c34db4385bfd77db64228d9220e481
usmanahm7 <usman.m.ahmad@metropolia.fi>
2026-08-27 10:27:06 +0300

    kotitehtävä

---
+++ mod01/Kotitehtävä.4.2.py
@@ -0,0 +1,17 @@
+hytti = (input("Mikä on hytti luokkasi?: "))
+
+LUX = "parvekkeellinen hytti yläkannella."
+A = "ikkunallinen hytti autokannen yläpuolella."
+B = "ikkunaton hytti autokannen yläpuolella."
+C = "ikkunaton hytti autokannen alapuolella."
+
+if hytti == "LUX":
+    print(LUX)
+elif hytti == "A":
+    print(A)
+elif hytti == "B":
+    print(B)
+elif hytti == "C":
+    print(C)
+else:
+    print("Tuntematon hyttiluokka. ")
