40bb37e2f7645917c3398231da5706fff572b291
usmanahm7 <usman.m.ahmad@metropolia.fi>
2026-08-27 15:05:39 +0300

    lasku

---
+++ mod01/mod05tuntitehtava.py
@@ -0,0 +1,7 @@
+while True:
+    menu_list ="Select option:\n1. add \n2. substract \n3. multiply \n0. exit"
+    selection = input(menu_list)
+    if selection == "0":
+        break
+    
+                
--- mod01/tuntitehtävä.mod05.py
+++ mod01/tuntitehtävä.mod05.py
@@ -1,1 +1,9 @@
-import math
+komento = input("Anna lasku: ")
+while komento != "lopeta":
+    if komento == "Virhe":
+        break
+    print("Suoritan toiminnon: " + komento)
+    komento = input("Anna komento: ")
+else:
+    print("Näkemiin.")
+print("Toiminnot lopetettu.")
