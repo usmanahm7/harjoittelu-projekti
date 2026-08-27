3d488c47bb31d28fb6717a23a0700822b6599c3d
usmanahm7 <usman.m.ahmad@metropolia.fi>
2026-08-27 13:17:14 +0300

    kotitehtävä

--- mod01/kotitehtävä.3.5.py
+++ mod01/kotitehtävä.3.5.py
@@ -1,3 +1,10 @@
 leiviskat = float(input("Anna leiviskät: "))
 naulat = float(input("Anna naulat: "))
 luodit = float(input("Anna luodit: "))
+
+paino = (leiviskat * 20 *32 +luodit) * 13.3
+kilot = paino // 1000
+grammat = paino % 1000
+
+print(f"Massa nykymittojen mukaan: ")
+print(f"{kilot} kilogramma ja {grammat:.2f} gramma. ")
