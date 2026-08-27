9fbba56a26e949fd6f7090221a4099dae8c8e10b
usmanahm7 <usman.m.ahmad@metropolia.fi>
2026-08-27 15:14:49 +0300

    laksu

--- mod01/mod05tuntitehtava.py
+++ mod01/mod05tuntitehtava.py
@@ -3,5 +3,13 @@ while True:
     selection = input(menu_list)
     if selection == "0":
         break
-    
-                
+
+numero1 = int(input("Anna numero 1"))
+numero2 = int(input("Anna numero 2"))
+    if selection == "1":
+        print(f"Vastaus: {numero1} + {numero2} = {numero1 + numero2}")
+    elif selection == "2":
+         print(f"Vastaus: {numero1} - {numero2} = {numero1 - numero2}")
+     elif selection == "3":
+          print(f"Vastaus: {numero1} + {numero2} = {numero1 + numero2}")  
+          
