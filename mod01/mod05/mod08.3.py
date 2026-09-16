6e7b12e6cc92e5ee58f85ec4ce28a375c397e331
usmanahm7 <usman.m.ahmad@metropolia.fi>
2026-09-16 13:55:09 +0300

    mod

---
+++ mod01/mod05/mod08.3.py
@@ -0,0 +1,12 @@
+asemat = []
+while True:
+    valinta = input("1.Lisää lentoasema \n2. Hae asemaa \n3.Lopeta")
+    if valinta == "1":
+        iceao = input("Anna asema ICEAO koodi:")
+        nimi = input("Anna asema nimi: ")
+        asemat[iceao] = nimi
+    if valinta == "2":
+        icao = input("Anna aseman ICAo koodi: ")
+        print(f"Aseman nimi on: {asemat[iceao]}")
+    if valinta == "3":
+        break
